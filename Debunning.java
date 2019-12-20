3f73955360427797d067a22d86956a7ca105d27b
shmkane <shmkane@gmail.com>
2019-12-20 13:55:29 -0500

    Update Debunning.java

--- Debunning.java
+++ Debunning.java
@@ -1,32 +1,41 @@
-import Universe.Galaxy.Earth.NA.NC.humans.Daniels;
+import Universe.Galaxy.Earth.NA.USA.NC.humans;
 
 public class Debunning extends Humans{
+	
+public static void main(String args[]) {
+    Daniel dan = HumanFinderAPI.findHuman("Dan K____");
+    Scissors s = new RustyScissors();
+    if(dan == null) {
+	    //Should never happen since this person exists.
+	    exit(1);
+    }
+  
+    try{
+    	deBun(dan, s);
+    }catch(AngryDanielException ade) {
+	    // Run!
+    }
+}
 
 public boolean isBunned(Daniel dan) {
     return dan.hasBun();
 }
 
-public void deBun(Daniel dan) {
+public void deBun(Daniel dan, Scissors s) throws AngryDanielException{
 
     if(isBunned(dan)) {
       
-        Bun b = (HoneyBun) dan.getBun(); //get casted to a honey bun nerd
-        Scissors s = new RustyScissors();
-          
-        try{
-             s.cut(b); //Later loser
-        }catch(AngryDaniel ad) {//Something's gone wrong, dan's mad!
-
-            System.out.println("Error: Something went wrong. Error Code: ");
-            System.out.println(ad.getExeption());
-        }
+        Bun b = (HoneyBun) dan.getBun(); // get casted to a honey bun nerd!
+        s.cut(b); //Later!
+		if(dan.isAnrgry)
+			throw new AngryDanielException("You call this a haircut?!");
 	
-        }else{
+     }else{
  
-            System.out.println("This is already a real man. No buns found here");
-            System.out.println("Look for a new Daniel BunManiel");
-        }
-  
+        System.out.println("This is already a real man. No buns found here");
+        System.out.println("Look for a new Daniel BunManiel");
     }
+  
+}
 
 }
